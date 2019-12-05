84552d6ce83d53c064502022bdb5efa1fb816b8a
Christian Amauger <christian.amauger@gmail.com>
2019-12-05 13:35:26 -0500

    sidebar test

--- wp-content/themes/understrap/sidebar-templates/sidebar-left.php
+++ wp-content/themes/understrap/sidebar-templates/sidebar-left.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The sidebar containing the main widget area.
  *
@@ -6,21 +7,30 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-if ( ! is_active_sidebar( 'left-sidebar' ) || is_shop()) {
+if (!is_active_sidebar('left-sidebar')) {
 	return;
 }
 
+
+
 // when both sidebars turned on reduce col size to 3 from 4.
-$sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
+$sidebar_pos = get_theme_mod('understrap_sidebar_position');
 ?>
 
-<?php if ( 'both' === $sidebar_pos ) : ?>
+<?php if ('both' === $sidebar_pos) : ?>
 	<div class="col-md-3 widget-area" id="left-sidebar" role="complementary">
-<?php else : ?>
-	<div class="col-md-4 widget-area" id="left-sidebar" role="complementary">
-<?php endif; ?>
-<?php dynamic_sidebar( 'left-sidebar' ); ?>
+	<?php else : ?>
+		<div class="col-md-4 widget-area" id="left-sidebar" role="complementary">
+		<?php endif;
+		if (is_shop()) {
+			dynamic_sidebar('right-sidebar');
+		} else {
+			dynamic_sidebar('left-sidebar');
+		}
+		?>
 
-</div><!-- #left-sidebar -->
+
+
+		</div><!-- #left-sidebar -->

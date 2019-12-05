6f8370755d02cbb9d730780e1d3439335a3af052
Christian Amauger <christian.amauger@gmail.com>
2019-12-05 10:44:33 -0500

    carousel test

--- wp-content/themes/understrap/modules/carousel.php
+++ wp-content/themes/understrap/modules/carousel.php
@@ -1,39 +1,49 @@
 <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-        <a href="<?php echo ($post->slideUrlOne) ?>">
-      <img src="<?php echo ($post->slideImgOne) ?>" class="d-block w-100" alt="<?php echo ($post->slideTitleOne) ?>">
-      <div class="carousel-caption d-none d-md-block">
-        <h5><?php echo ($post->slideTitleOne) ?></h5>
-        <p><?php echo ($post->slideTextOne) ?></p></a>
-      </div>
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <a href="<?php echo ($post->slideUrlOne) ?>">
+                <img src="<?php echo ($post->slideImgOne) ?>" class="d-block w-100" alt="<?php echo ($post->slideTitleOne) ?>">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5><?php echo ($post->slideTitleOne) ?></h5>
+                    <p><?php echo ($post->slideTextOne) ?></p>
+            </a>
+        </div>
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-      </div>
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <a href="<?php echo ($post->slideUrlOne) ?>">
+                <div
+                class="container">
+                <div class="row">
+                <div class="col-6">
+                <img src="<?php echo ($post->slideImgOne) ?>" class="d-block w-100" alt="<?php echo ($post->slideTitleOne) ?>">
+                </div>
+                <div class="col-6">
+                    <h5><?php echo ($post->slideTitleOne) ?></h5>
+                    <p><?php echo ($post->slideTextOne) ?></p>
+                </div>
+            </div>
+            </a>
+        </div>
+        <div class="carousel-item active">
+            <a href="<?php echo ($post->slideUrlOne) ?>">
+                <img src="<?php echo ($post->slideImgOne) ?>" class="d-block w-100" alt="<?php echo ($post->slideTitleOne) ?>">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5><?php echo ($post->slideTitleOne) ?></h5>
+                    <p><?php echo ($post->slideTextOne) ?></p>
+            </a>
+        </div>
+</div>
+<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+</a>
+<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
-  </a>
+</a>
 </div>

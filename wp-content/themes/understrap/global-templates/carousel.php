41e7bce3b405586c1bd3bfbbb754a9ff2cbdd8b2
Christian Amauger <christian.amauger@gmail.com>
2019-12-05 10:01:50 -0500

    templates

---
+++ wp-content/themes/understrap/global-templates/carousel.php
@@ -0,0 +1,122 @@
+<!-- Carousel Homepage -->
+<!-- <div id="carousel-example-generic-1" class="carousel slide carousel-accueil" data-ride="carousel">
+    <div class="carousel-inner" role="listbox">
+
+        <div class="item active">
+
+
+            <img class="img-responsive"
+                src="https://seigneurieiledorleans.com/contenu/prospecteur/uploads/2018/06/bandeau-2019.png" alt=""
+                title="">
+
+        </div>
+
+        <div class="item">
+            <img class="img-responsive" src="<?php echo get_post_meta($post->ID, 'homeslider1', true); ?>" alt=""
+                title="">
+        </div>
+
+    </div>
+    <a class="left carousel-control" href="#carousel-example-generic-1" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#carousel-example-generic-1" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div> -->
+<!-- end of carousel -->
+<!-- <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="<?php echo get_post_meta($post->ID, 'slide1', true); ?>" alt="First slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="<?php echo get_post_meta($post->ID, 'slide2', true); ?>" alt="Second slide">
+            <div class="carousel-caption caption-left d-none d-md-block">
+                <h5><?php echo get_post_meta($post->ID, 'slide2-title', true); ?></h5>
+                <p><?php echo get_post_meta($post->ID, 'slide2-caption', true); ?></p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="<?php echo get_post_meta($post->ID, 'slide3', true); ?>" alt="Third slide">
+            <div class="carousel-caption caption-right d-none d-md-block">
+                <h5><?php echo get_post_meta($post->ID, 'slide3-title', true); ?></h5>
+                <p><?php echo get_post_meta($post->ID, 'slide3-caption', true); ?></p>
+            </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div> -->
+
+
+<!-- CAROUSEL PRODUITS -->
+<div class="carousel-accueil-produits">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+
+        <div class="carousel-inner" role="listbox">
+
+            <!-- <div class="carousel-item active">
+
+                <img class="img-fluid" src="<?php echo get_post_meta($post->ID, 'slide1', true); ?>" />
+
+                <div class="carousel-caption d-none d-md-block">
+                    <div class="inner-content-carousel-produit">
+                        <h4><?php echo get_post_meta($post->ID, 'slide1-title', true); ?></h4>
+                        <p><?php echo get_post_meta($post->ID, 'slide1-caption', true); ?></p>
+                        <a href="<?php echo get_post_meta($post->ID, 'slide1-link', true); ?>"
+                            class="btn btn-primary btn-vert pull-right"><?php echo get_post_meta($post->ID, 'slide1-button', true); ?></a>
+                    </div>
+                </div>
+            </div> -->
+
+            <div class="carousel-item active">
+
+                <img style="float: right" class="img-fluid" src="<?php echo get_post_meta($post->ID, 'slide2', true); ?>" />
+
+                <div class="carousel-caption d-none d-md-block">
+                    <div style="max-width: 300px" class="inner-content-carousel-produit text-left">
+                        <h4><?php echo get_post_meta($post->ID, 'slide2-title', true); ?></h4>
+                        <p><?php echo get_post_meta($post->ID, 'slide2-caption', true); ?></p>
+                        <a href="<?php echo get_post_meta($post->ID, 'slide2-link', true); ?>"
+                            class="btn btn-primary btn-vert pull-left"><?php echo get_post_meta($post->ID, 'slide2-button', true); ?></a>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+
+                <img style="float: right; max-height: 300px; padding: 20px;" class="img-fluid" src="<?php echo get_post_meta($post->ID, 'slide3', true); ?>" />
+
+                <div class="carousel-caption d-none d-md-block">
+                    <div style="max-width: 300px" class="inner-content-carousel-produit text-right">
+                        <h4><?php echo get_post_meta($post->ID, 'slide3-title', true); ?></h4>
+                        <p><?php echo get_post_meta($post->ID, 'slide3-caption', true); ?></p>
+                        <a href="<?php echo get_post_meta($post->ID, 'slide3-link', true); ?>"
+                            class="btn btn-primary btn-vert pull-right"><?php echo get_post_meta($post->ID, 'slide3-button', true); ?></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.carousel-inner -->
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic-2" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic-2" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <!-- /.carousel-example-generic-2 -->
+</div>
+<!-- /.carousel -->
---
+++ wp-content/themes/understrap/page-home.php
@@ -0,0 +1,104 @@
+<?php
+/**
+* Template Name: Accueil
+*
+*
+* @package understrap
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header();
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+
+
+
+
+
+<div class="wrapper" id="index-wrapper">
+
+    <div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+        <div class="row">
+
+
+
+            <main class="site-main" id="main">
+                <?php get_template_part( 'global-templates/carousel' ); ?>
+                <div class="home-cards-wrapper">
+                    <div class="home-cards">
+                        <div class="home-cards__half">
+                            <!-- première moitié de la grille -->
+                            <div class="row">
+                                <div class="box--large-1">
+                                    <a href="/activites">
+                                        <span>Prenez part</span>
+                                        <strong>aux activités</strong>
+                                    </a>
+                                </div>
+                                <div class="box--small-1">
+                                    <a href="https://www.facebook.com/seigneurieiledorleans/">
+                                        <i class="fa fa-facebook"></i>
+                                        <span>Suivez-nous</span>
+                                        <strong>sur Facebook</strong>
+                                    </a>
+                                </div>
+                                <div class="box--small-2">
+                                    <a href="#">
+                                        <i class="fa fa-paper-plane"></i>
+                                        <span>Inscrivez-vous</span>
+                                        <strong>à l'infolettre</strong>
+                                    </a>
+
+                                </div>
+                                <div class="box--large-2">
+                                    <a href="/la-lavande">
+                                        <span>Apprenez-en plus</span>
+                                        <strong>sur la lavande</strong>
+                                    </a>
+                                </div>
+                            </div>
+                        </div> <!-- fin de la première moitié de la grille -->
+                        <div class="home-cards__quarter shop">
+                            <a href="/boutique">
+                                <span class="mauve">La boutique</span>
+                                <strong class="mauve">en ligne</strong>
+                                <img src="https://seigneurieiledorleans.com/contenu/prospecteur/themes/prospecteur/images/accueil/sio-bg-boutique.jpg"
+                                    class="img-responsive" alt="La boutique en ligne" title="La boutique en ligne">
+                                <span class="uppercase primary">Livraison gratuite</span>
+                                <span>à l'achat de 80$ et +<br>
+                                    <small>avant taxes</small> </span>
+                            </a>
+
+                        </div>
+                        <div class="home-cards__quarter visit">
+                            <a href="/la-seigneurie/visites-et-forfaits/">
+                                <span>Planifiez une visite pour découvrir</span>
+                                <strong>la Seigneurie</strong>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container">
+                    <?php get_template_part( 'loop-templates/content', 'page' ); ?>
+                </div>
+
+
+
+            </main><!-- #main -->
+
+
+
+        </div><!-- .row -->
+
+    </div><!-- #content -->
+
+</div><!-- #index-wrapper -->
+
+<?php get_footer(); ?>

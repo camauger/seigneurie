3ff1b2a6abbd8e538519f4146648d8e0600bda74
Christian Amauger <christian.amauger@gmail.com>
2019-12-05 12:29:56 -0500

    footer update

--- wp-content/themes/understrap/footer.php
+++ wp-content/themes/understrap/footer.php
@@ -33,32 +33,9 @@ $container = get_theme_mod('understrap_container_type');
 							<div class="col-md-12">
 								<div class="visible-lg-block">
 									<img class="img-responsive bg-footer" src="<?php bloginfo('template_directory'); ?>/images/bg-bottom-2.png" alt="" title="">
-									<div class="contenu-footer hidden-sm">
-										<p><?php echo (ICL_LANGUAGE_CODE == 'en') ? 'All rights reserved' : 'Tous droits réservés'; ?> &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> -
-											<a href="mailto:info@seigneurieiledorleans.com" title="info@seigneurieiledorleans.com">info@seigneurieiledorleans.com</a> -
-											<?php echo (ICL_LANGUAGE_CODE == 'en') ? 'Tel' : 'Tél'; ?>. : 418 829-0476</p>
-									</div>
 								</div>
-								<div class="visible-md-block">
-									<img class="center-block img-responsive" style="margin-top: -3em;" src="<?php bloginfo('template_directory'); ?>/images/sio-logo-footer.png" alt="" title="">
-									<div class="text-center">
-										<p>Tous droits réservés &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> -
-											<a style="color: #fff;" href="mailto:info@seigneurieiledorleans.com" title="info@seigneurieiledorleans.com">info@seigneurieiledorleans.com</a> -
-											Tél. : <a style="color: #fff !important;" tabIndex="-1" href="tel:14188290476">418 829-0476</a></p>
-									</div>
-								</div>
-								<div class="visible-sm-block">
-									<div class="text-center">
-										<p style="color: #fff; padding-top: .5em;"><?php bloginfo('name'); ?>
-											<br />
-											<?php echo (ICL_LANGUAGE_CODE == 'en') ? 'All rights reserved' : 'Tous droits réservés'; ?> &copy; <?php echo date('Y'); ?>
-											<br />
-											<a style="color: #fff;" href="mailto:info@seigneurieiledorleans.com" title="info@seigneurieiledorleans.com">info@seigneurieiledorleans.com</a>
-											<br />
-											<a style="color: #fff !important;" tabIndex="-1" href="tel:14188290476">418 829-0476</a>
-										</p>
-									</div>
-								</div>
+
+
 								<div class="visible-xs-block">
 									<div class="text-center">
 										<p style="color: #fff; padding-top: .5em;"><?php bloginfo('name'); ?>
@@ -76,7 +53,7 @@ $container = get_theme_mod('understrap_container_type');
 					</div>
 				</footer>
 
-				
+
 
 			</div>
 			<!--col end -->

565820e82244f4727c793a29d9162a873d22e3ca
Christian Amauger <christian.amauger@gmail.com>
2019-12-05 10:58:11 -0500

    carousel

--- wp-content/themes/understrap/modules/carousel.php
+++ wp-content/themes/understrap/modules/carousel.php
@@ -5,46 +5,41 @@
         <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner">
-        <!-- <div class="carousel-item active">
-            <a href="<?php echo ($post->slideUrlOne) ?>">
-                <img src="<?php echo ($post->slideImgOne) ?>" class="d-block w-100" alt="<?php echo ($post->slideTitleOne) ?>">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5><?php echo ($post->slideTitleOne) ?></h5>
-                    <p><?php echo ($post->slideTextOne) ?></p>
-            </a>
-        </div> -->
-
         <div class="carousel-item active">
             <a href="<?php echo ($post->slideUrlOne) ?>">
-                <div class="">
-                    <div class="">
-                        <div class="">
-                            <img src="<?php echo ($post->slideImgOne) ?>" class="d-block w-100" alt="<?php echo ($post->slideTitleOne) ?>">
-                        </div>
-                        <div class="">
-                            <h5><?php echo ($post->slideTitleOne) ?></h5>
-                            <p><?php echo ($post->slideTextOne) ?></p>
-                        </div>
-                    </div>
+                <img src="<?php echo ($post->slideImgOne) ?>" class="d-block w-100" alt="<?php echo ($post->slideTitleOne) ?>">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5><?php echo ($post->slideTitleOne) ?></h5>
+                    <p><?php echo ($post->slideTextOne) ?></p>
+                </div>
             </a>
         </div>
-        <!-- <div class="carousel-item">
+        <div class="carousel-item">
+            <a href="<?php echo ($post->slideUrlOne) ?>">
+                <img src="<?php echo ($post->slideImgOne) ?>" class="d-block" alt="<?php echo ($post->slideTitleOne) ?>">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5><?php echo ($post->slideTitleOne) ?></h5>
+                    <p><?php echo ($post->slideTextOne) ?></p>
+                </div>
+            </a>
+        </div>
+        <div class="carousel-item">
             <a href="<?php echo ($post->slideUrlOne) ?>">
                 <img src="<?php echo ($post->slideImgOne) ?>" class="d-block w-100" alt="<?php echo ($post->slideTitleOne) ?>">
                 <div class="carousel-caption d-none d-md-block">
                     <h5><?php echo ($post->slideTitleOne) ?></h5>
                     <p><?php echo ($post->slideTextOne) ?></p>
-            </a> -->
-        <!-- </div> -->
+                </div>
+            </a>
+        </div>
     </div>
-</div>
 
-<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-</a>
-<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-</a>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
 </div>
